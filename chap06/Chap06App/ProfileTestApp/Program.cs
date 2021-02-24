484c8e169036908586124815f03aee55cc631cce
yfla980107 <yfla980107@gmail.com>
2021-02-24 10:41:32 +0900

    21/02/24 6장

--- chap06/Chap06App/ProfileTestApp/Program.cs
+++ chap06/Chap06App/ProfileTestApp/Program.cs
@@ -16,9 +16,13 @@ namespace ProfileTestApp
             {
                 Console.WriteLine("프로필 출력시 오류가 발생했습니다.");
             }
+
+            // PrintProfile(phone: "010-9999-9999", name: "홍길동"); // 순서가 바뀌어도 자동으로 할당
+            // PrintProfile(name: "홍길순", phone: "010-0000-0000"); //name, phone은 자동으로 지워줌, 쓸필요없음
+            PrintProfile("최백호");
         }
 
-        public static int PrintProfile(string name, string phone)
+        public static int PrintProfile(string name, string phone = "010-7979-7979")
         {
             if(string.IsNullOrEmpty(name))
             {
