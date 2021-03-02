f7c254204287d43c2fabb9622ad1d827934cae15
yfla980107 <yfla980107@gmail.com>
2021-03-02 17:24:53 +0900

    21/03/02 13장

---
+++ chap13/Chap13App/DelegateChainApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DelegateChainApp
+{
+    delegate void AllCalc(int x, int y); //대리자 선언
+    class Program
+    {
+        static void Plus(int a, int b) { Console.WriteLine($"a + b = {a + b}"); }
+        static void Minus(int a, int b) { Console.WriteLine($"a + b = {a - b}"); }
+        static void Multiple(int a, int b) { Console.WriteLine($"a + b = {a * b}"); }
+        static void Divide(int a, int b) { Console.WriteLine($"a + b = {a / b}"); }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculating");
+
+            //대리자 사용시
+            AllCalc allCalc = Plus;
+            allCalc += Minus;
+            allCalc += Multiple;
+            allCalc += Divide;
+
+            allCalc(10, 5);
+
+            //대리자 사용안할시
+            /*Plus(10, 5);
+            Minus(10, 5);
+            Multiple(10, 5);
+            Divide(10, 5);*/
+
+            Console.WriteLine("곱셈 메서드 제거");
+            allCalc -= Multiple;
+            allCalc(10, 5);
+        }
+    }
+}
